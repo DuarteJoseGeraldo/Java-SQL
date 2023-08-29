1438ea5b3e9fc5458c6b6c49bfb428d37e5766c1
DuarteJoseGeraldo <124914532+DuarteJoseGeraldo@users.noreply.github.com>
2023-08-29 13:10:35 -0300

    ainda com problema

--- src/main/java/com/example/JavaSQL/JavaSqlApplication.java
+++ src/main/java/com/example/JavaSQL/JavaSqlApplication.java
@@ -3,15 +3,30 @@ package com.example.JavaSQL;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
 
 @EnableFeignClients
 @SpringBootApplication
 public class JavaSqlApplication {
+    @Bean
+    JedisConnectionFactory jedisConnectionFactory() {
+        return new JedisConnectionFactory();
+    }
+
+    @Bean
+    public RedisTemplate<String, Object> redisTemplate() {
+        RedisTemplate<String, Object> template = new RedisTemplate<>();
+        template.setConnectionFactory(jedisConnectionFactory());
+        return template;
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(JavaSqlApplication.class, args);
+
     }
 
 }
